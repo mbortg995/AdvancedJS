5d1ac33fd804193d41120edb8dca574ebce95262
mbortg995 <mbortg995@gmail.com>
2024-11-11 20:37:05 +0100

    First front web that connects to an external API to obtain a random useles fact each time you click a button.
    Without REACT, just DOM and PROMISES.

--- 03_PROMISES/main.js
+++ 03_PROMISES/main.js
@@ -1,12 +1,27 @@
 const endpoint = 'https://uselessfacts.jsph.pl/api/v2/facts/random';
 
 async function main(){
-  const response = await fetch(endpoint);
-  const random_fact = await response.json();
-  console.log('Random Fact', random_fact);
+
+
 
   const main_div = document.getElementById('root');
-  main_div.innerText = random_fact.text;
+
+  const random_fact_div = document.createElement('div');
+  main_div.appendChild(random_fact_div);
+  random_fact_div.setAttribute('id', 1);
+
+  const number_button = document.createElement('button');
+  main_div.appendChild(number_button);
+  number_button.innerText = 'Get Random Fact';
+  number_button.addEventListener('click', () => getrandomfact());
+
+}
+
+async function getrandomfact(){
+  const response = await fetch(endpoint);
+  const random_fact = await response.json();
+  const random_fact_div = document.getElementById(1);
+  random_fact_div.innerText = random_fact.text;
 }
 
 
